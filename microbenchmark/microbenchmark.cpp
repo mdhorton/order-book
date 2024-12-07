59f612f9472751953f99fcc31467ad3f5f377a70
mdhorton <mark@nostromo.net>
2024-12-07 18:55:34 +0000

    working on itch

--- microbenchmark/microbenchmark.cpp
+++ microbenchmark/microbenchmark.cpp
@@ -3,17 +3,22 @@
 #include "order_book_1/order_book.hpp"
 
 namespace nostromo::order_book::order_book_1 {
-static void BM_order_book_1_bid_cancel(benchmark::State &state) {
-   printf("%lu\n", sizeof(NewOrder));
-   const auto base_price = std::rand() % 100000 + 300000;
+static void BM_order_book_1_bid_add_delete(benchmark::State &state) {
+   const auto base_price = std::rand() % 100'000 + 300'000;
    const auto base_order_id = std::rand() % 1000;
+   constexpr auto price_mask = (1 << 17) - 1; // 128k
+   constexpr auto order_id_mask = (1 << 10) - 1; // 1k
+   uint64_t iters = 0;
+
    OrderBook book{100, 2500};
 
    for (auto _: state) {
-      const auto price = static_cast<int32_t>(state.iterations() % 100000 + base_price);
-      const auto order_id = static_cast<uint32_t>(state.iterations() % 10000 + base_order_id);
-      book.HandleNewOrder(NewOrder{price, order_id, 1, 1, true});
-      book.HandleCancelOrder(CancelOrder{order_id});
+      ++iters;
+      const auto price = static_cast<uint32_t>((iters & price_mask) + base_price);
+      const auto order_id = static_cast<uint32_t>((iters & order_id_mask) + base_order_id);
+      book.OrderAdd(ItchOrderAdd{1, 1, order_id, 1, 1, price});
+      book.OrderDelete(ItchOrderDelete{1, 1, order_id});
+      benchmark::ClobberMemory();
    }
 
    benchmark::DoNotOptimize(book.BestBid());
@@ -27,11 +32,12 @@ static void BM_div_32(benchmark::State &state) {
       constexpr int32_t div = 100;
       ++p1;
       benchmark::DoNotOptimize(r1 = p1 / div - p1);
+      benchmark::ClobberMemory();
    }
 
    benchmark::DoNotOptimize(r1);
 }
 
-BENCHMARK(BM_order_book_1_bid_cancel)->MinTime(1);
+BENCHMARK(BM_order_book_1_bid_add_delete);
 BENCHMARK(BM_div_32);
 } // nostromo::order_book::order_book_1
